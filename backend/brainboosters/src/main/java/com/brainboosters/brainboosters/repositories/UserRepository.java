efe79efa6f39868e1a84eaa5ed52907a850b8c59
HimashRajapaksha <rajapakshahimash@gmail.com>
2025-04-25 18:07:58 +0530

    fix: repository remove

--- backend/brainboosters/src/main/java/com/brainboosters/brainboosters/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brainboosters.brainboosters.repositories;
-
-import com.brainboosters.brainboosters.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    // Custom query to find a user by email
-    User findByEmail(String email);
-}
