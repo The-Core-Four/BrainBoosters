c48d5dbc2107e8299f59d94d84f68868646e693c
HimashRajapaksha <rajapakshahimash@gmail.com>
2025-05-07 19:58:18 +0530

    fix: firebase console

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCDP_N9fYjlJnshuc4jckOP5IMo0HSrbmo",
   authDomain: "pafmain-a0be6.firebaseapp.com",
@@ -11,13 +12,14 @@ const firebaseConfig = {
   appId: "1:136832715572:web:ac62d5fc2f9bb35cfff361"
 };
 // const firebaseConfig = {
-//   apiKey: "AIzaSyAX7I2K0Rmi12NyWG6HRmnKo3a_fRwkSsc",
-//   authDomain: "brainboosters-d2872.firebaseapp.com",
-//   projectId: "brainboosters-d2872",
-//   storageBucket: "brainboosters-d2872.firebasestorage.app",
-//   messagingSenderId: "316278308724",
-//   appId: "1:316278308724:web:24483777e3a6eb6f2ff621"
+//   apiKey: "AIzaSyAEpBvNjMtNuSHkwIWnyikPBExvTAi-QU8",
+//   authDomain: "uee-project-7e802.firebaseapp.com",
+//   projectId: "uee-project-7e802",
+//   storageBucket: "uee-project-7e802.appspot.com",
+//   messagingSenderId: "513594411651",
+//   appId: "1:513594411651:web:6d7a0b216431a283dc29b6"
 // };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
